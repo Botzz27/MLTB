73ebcd3a208d44d517f851d0dbab0b9809b40a84
Botzz27 <132325753+Botzz27@users.noreply.github.com>
2023-05-02 11:25:03 +0530

    Update update.py

--- update.py
+++ update.py
@@ -12,8 +12,8 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-UPSTREAM_REPO = 'https://github.com/Botzz27/MLTB'
-UPSTREAM_BRANCH = 'update'
+UPSTREAM_REPO = ''
+UPSTREAM_BRANCH = ''
 
 if UPSTREAM_REPO is not None:
     if ospath.exists('.git'):
